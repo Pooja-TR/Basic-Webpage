62495e5a210afbc72c17dfaa0bf7df17b50a47d4
Pooja-TR <trpooja978@gmail.com>
2020-10-03 11:29:09 +0530

    Adding Background Generator JS

---
+++ script.js
@@ -0,0 +1,17 @@
+var gradInfo = document.querySelector("h3")
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function bodyBackground (){
+	body.style.background = "linear-gradient(to right , " 
+	+ color1.value +" , " 
+	+ color2.value +")";
+
+	gradInfo.textContent = body.style.background + ";";
+}
+
+// color1.addEventListener("input", bodyBackground)
+
+// color2.addEventListener("input", bodyBackground)
+
